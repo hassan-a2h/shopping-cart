0ef75d598b83682d1e20d593da79eaa565cd78c4
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-11-20 14:16:58 +0500

    import products from fakestore api, use hooks to render data
    Edits:
    * create products as state, fetch data from api and assign it to products
    * each fetched product gets rendered using Product component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
 import Nav from './Navbar';
+import Product from './Product';
 
-const Shop = () => {
+const Shop = ({ category }) => {
+  let [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/category/jewelery`, 
+      { mode: 'cors' })
+    .then(response => { 
+      if (!response.ok) {
+        throw new Error("Error, couldn't load data");
+      }
+      return response.json();
+    })
+    .then(response => setProducts(response))
+    .catch(error => console.log(error));
+  }, []);
+
   return (
-    <Nav leftItems={['Home']} rightItems={['Checkout']}/>
-    
+    <>
+      <Nav leftItems={['Home']} rightItems={['Checkout']}/>
+      {products && products.map(product => <Product product={product}/>)}
+    </>
   );
 };
 
